088e5b540aba2e59cf3d5e822bf32512e4271dbe
Łukasz Szerszyński <lukiszerszen@gmail.com>
2023-08-12 00:55:18 +0200

    simple structur

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,17 @@ import {
 } from 'react-router-dom';
 import './App.scss'
 import './menu/navBar.scss'
+import './header/header.scss'
+import Header from'./header/header'
 import NavBar from './menu/navBar';
 
 
 function App() {
   return (
-     <div className='menu'><NavBar /></div>
-    
+    <>
+     <div className='menu'><NavBar /></div>;
+    <div><Header /></div>
+    </>
   );
 }
 
--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,1 +1,13 @@
 import React from "react";
+
+export default function header () {
+    return (
+        <div className="card">
+            <h3 className="card__text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+            <div className="card__btns">
+                <div className="card__btn"></div>
+                <div className="card__btn"></div>
+            </div>
+        </div>
+    )
+  }
